462df6fc0a8905e68141c21a2281f8299eb8cbb8
setsuodu <339131001@qq.com>
2020-01-31 21:07:48 +0800

    添加luasocket使用案例

---
+++ Assets/XLua/Examples/16_LuaSocket/LuaSocket.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using XLua;
+
+namespace XLuaTest
+{
+    public class LuaSocket : MonoBehaviour
+    {
+        void Start()
+        {
+            LuaEnv luaenv = new LuaEnv();
+            luaenv.DoString("require 'LuaSocketTest'"); //不用手动AddBuildin，已经在官方LuaEnv中内置了
+            luaenv.Dispose();
+        }
+    }
+}
